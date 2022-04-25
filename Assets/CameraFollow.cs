247f4a18303444f4f18eda8d5fd08e8ad5dfd990
Perinamer1 <edvin.perinamer@gmail.com>
2022-04-25 10:36:27 +0200

    Init

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Controller.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Controller : MonoBehaviour
+{
+    [HideInInspector] public Vector2 moveInput; public void OnMove(InputAction.CallbackContext ctx) => moveInput = ctx.ReadValue<Vector2>();
+    [HideInInspector] public Vector2 lookInput; public void OnLook(InputAction.CallbackContext ctx) => lookInput = ctx.ReadValue<Vector2>();
+    [HideInInspector] public bool useInput; public void OnUse(InputAction.CallbackContext ctx) => useInput = ctx.action.triggered;
+}
